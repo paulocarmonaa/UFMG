380b53c7ce25cace5b5e94ccd7e22707dfd835f4
Paulo Henrique <paulo.carmona17@gmail.com>
2025-06-21 20:13:58 -0300

    Alterações do TAD minHeap para adequar sua estrutura e organização

--- ED/TP2/include/minHeap.hpp
+++ ED/TP2/include/minHeap.hpp
@@ -1,35 +1,34 @@
 #ifndef MINHEAP_HPP
 #define MINHEAP_HPP
 
-#include "Evento.hpp" // Certifique-se de que este arquivo exista e defina a struct Evento
+#include "Evento.hpp" 
 
-// Classe template para o Min-Heap, que funcionará como uma fila de prioridade.
-// O tipo 'T' será 'Evento*' para o escalonador do seu trabalho.
+
 template <typename T>
 class MinHeap {
-private:
-    T* array;
-    int capacidade;
-    int tamanho;
+    private:
+        T* array;
+        int capacidade;
+        int tamanho;
 
-    // Métodos auxiliares privados para as operações do heap.
-    void heapifyUp(int index);
-    void heapifyDown(int index);
-    void redimensionar();
-    int getPai(int index);
-    int getFilhoEsquerda(int index);
-    int getFilhoDireita(int index);
-    void trocar(int i, int j);
+        // Métodos auxiliares privados para as operações do heap
+        void heapifyUp(int index);
+        void heapifyDown(int index);
+        void redimensionar();
+        int getPai(int index);
+        int getFilhoEsquerda(int index);
+        int getFilhoDireita(int index);
+        void trocar(int i, int j);
 
-public:
-    // Construtor e Destrutor.
-    MinHeap();
-    ~MinHeap();
+    public:
+        // Construtor e Destrutor.
+        MinHeap();
+        ~MinHeap();
 
-    // Métodos públicos da interface.
-    void inserir(T item);
-    T removerMin();
-    bool estaVazio() const;
+        // Métodos públicos da interface.
+        void inserir(T item);
+        T removerMin();
+        bool estaVazio() const;
 };
 
-#endif // MINHEAP_HPP
+#endif
--- ED/TP2/src/minHeap.cpp
+++ ED/TP2/src/minHeap.cpp
@@ -1,62 +1,72 @@
 #include "minHeap.hpp"
-#include <stdexcept> // Necessário para std::runtime_error
+#include <stdexcept>
 
-// --- Implementação do Construtor ---
+//Método para garantir a propriedade do MinHeap
+//Troca um nó pelo seu pai, "subindo-o" na árvore até a condição do filho ser maior que o pai ser atingida
 template <typename T>
-MinHeap<T>::MinHeap() : capacidade(10), tamanho(0) {
-    array = new T[capacidade];
+void MinHeap<T>::heapifyUp(int index) {
+    while (index > 0 && *array[index] < *array[getPai(index)]) {
+        trocar(index, getPai(index));
+        index = getPai(index);
+    }
 }
 
-// --- Implementação do Destrutor ---
+//Método para garantir a propriedade do MinHeap
+//"Afunda" um elemento na árvore quando o mínimo é retirado para garantir a propriedade do MinHeahp do
+// pai ser sempre menor que o filho
 template <typename T>
-MinHeap<T>::~MinHeap() {
+void MinHeap<T>::heapifyDown(int index) {
+    int menorIndex;
+
+    while (true) {
+        menorIndex = index;
+        int esq = getFilhoEsquerda(index);
+        int dir = getFilhoDireita(index);
+
+        if (esq < tamanho && *array[esq] < *array[menorIndex]) 
+            menorIndex = esq;
+        if (dir < tamanho && *array[dir] < *array[menorIndex]) 
+            menorIndex = dir;
+        if (index == menorIndex) 
+            break;
+
+        trocar(index, menorIndex);
+        index = menorIndex;
+    }
+}
+
+//Redimensiona a capacidade do array que armazena o heap quando ele fica cheio
+template <typename T>
+void MinHeap<T>::redimensionar() {
+    capacidade *= 2;
+    T* novoArray = new T[capacidade];
+
+    for (int i = 0; i < tamanho; ++i) 
+        novoArray[i] = array[i];
+    
     delete[] array;
+    array = novoArray;
 }
 
-// --- Implementação dos Métodos Públicos ---
-template <typename T>
-void MinHeap<T>::inserir(T item) {
-    if (tamanho == capacidade) {
-        redimensionar();
-    }
-    array[tamanho] = item;
-    tamanho++;
-    heapifyUp(tamanho - 1);
-}
-
-template <typename T>
-T MinHeap<T>::removerMin() {
-    if (estaVazio()) {
-        throw std::runtime_error("Erro: Heap vazio ao tentar remover o minimo.");
-    }
-    T itemMinimo = array[0];
-    array[0] = array[tamanho - 1];
-    tamanho--;
-    heapifyDown(0);
-    return itemMinimo;
-}
-
-template <typename T>
-bool MinHeap<T>::estaVazio() const {
-    return tamanho == 0;
-}
-
-// --- Implementação dos Métodos Auxiliares ---
+//Retorna o pai do nó
 template <typename T>
 int MinHeap<T>::getPai(int index) {
     return (index - 1) / 2;
 }
 
+//Retorna o filho da esquerda do nó
 template <typename T>
 int MinHeap<T>::getFilhoEsquerda(int index) {
     return 2 * index + 1;
 }
 
+//Retorna o filho da direita do nó
 template <typename T>
 int MinHeap<T>::getFilhoDireita(int index) {
     return 2 * index + 2;
 }
 
+//Troca duas posicões do array que armazena o heap
 template <typename T>
 void MinHeap<T>::trocar(int i, int j) {
     T temp = array[i];
@@ -64,53 +74,48 @@ void MinHeap<T>::trocar(int i, int j) {
     array[j] = temp;
 }
 
+//Construtor
 template <typename T>
-void MinHeap<T>::heapifyUp(int index) {
-    // Compara os objetos (*ponteiro) para ordenar pela prioridade do evento,
-    // e não pelo endereço de memória do ponteiro.
-    while (index > 0 && *array[index] < *array[getPai(index)]) {
-        trocar(index, getPai(index));
-        index = getPai(index);
-    }
+MinHeap<T>::MinHeap() : capacidade(10), tamanho(0) {
+    array = new T[capacidade];
 }
 
+//Destrutor
 template <typename T>
-void MinHeap<T>::heapifyDown(int index) {
-    int menorIndex;
-    while (true) {
-        menorIndex = index;
-        int esq = getFilhoEsquerda(index);
-        int dir = getFilhoDireita(index);
-
-        // Compara os objetos (*ponteiro) para encontrar o menor.
-        if (esq < tamanho && *array[esq] < *array[menorIndex]) {
-            menorIndex = esq;
-        }
-        if (dir < tamanho && *array[dir] < *array[menorIndex]) {
-            menorIndex = dir;
-        }
-
-        if (index == menorIndex) {
-            break; // A propriedade do heap foi restaurada.
-        }
-        trocar(index, menorIndex);
-        index = menorIndex;
-    }
-}
-
-template <typename T>
-void MinHeap<T>::redimensionar() {
-    capacidade *= 2;
-    T* novoArray = new T[capacidade];
-    for (int i = 0; i < tamanho; ++i) {
-        novoArray[i] = array[i];
-    }
+MinHeap<T>::~MinHeap() {
     delete[] array;
-    array = novoArray;
 }
 
-// --- Instanciação Explícita ---
-// Esta linha é OBRIGATÓRIA para que o linker encontre a implementação
-// de MinHeap para o tipo Evento*. Sem ela, você terá um erro de
-// "undefined reference" ao compilar o projeto com arquivos separados.
+//Insere um item na estrutura do Heap
+template <typename T>
+void MinHeap<T>::inserir(T item) {
+    if (tamanho == capacidade)
+        redimensionar();
+    
+    array[tamanho] = item;
+    tamanho++;
+    heapifyUp(tamanho - 1);
+}
+
+//Remove o elemento mínimo(raiz) do Heap
+template <typename T>
+T MinHeap<T>::removerMin() {
+    if (estaVazio()) 
+        throw std::runtime_error("Erro: Heap vazio ao tentar remover o minimo.");
+    
+    T itemMinimo = array[0];
+    array[0] = array[tamanho - 1];
+    tamanho--;
+    heapifyDown(0);
+
+    return itemMinimo;
+}
+
+//Confere se o Heap está vazio
+template <typename T>
+bool MinHeap<T>::estaVazio() const {
+    return tamanho == 0;
+}
+
+// Instanciação Explícita
 template class MinHeap<Evento*>;
