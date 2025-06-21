3394a4b7695fe09f8ccb5e35ab31cfa4f1e14498
Paulo Henrique <paulo.carmona17@gmail.com>
2025-06-21 17:13:32 -0300

    Alterações do TAD ListaEncadeada para adequar sua estrutura e organização

--- ED/TP2/include/ListaEncadeada.hpp
+++ ED/TP2/include/ListaEncadeada.hpp
@@ -3,43 +3,162 @@
 
 #include <stdexcept>
 
-// Nó genérico da lista
+// Estrutura do nó
 template <typename T>
 struct No {
     T valor;
     No<T>* proximo;
     No<T>* anterior;
 
+    // Construtor do nó
     No(T val) : valor(val), proximo(nullptr), anterior(nullptr) {}
 };
 
-// Lista duplamente encadeada
 template <typename T>
 class ListaEncadeada {
-private:
-    No<T>* cabeca;
-    No<T>* cauda;
-    int _tamanho;
+    private:
+        No<T>* cabeca;
+        No<T>* cauda;
+        int tamanho;
 
-public:
-    ListaEncadeada();
-    ~ListaEncadeada();
+    public:
+        // Construtor e destrutor
+        ListaEncadeada();
+        ~ListaEncadeada();
 
-    void inserirInicio(T valor);
-    void inserirFim(T valor);
-    T removerInicio();
-    T removerFim();
-    T obterPrimeiro() const;
-    T obterUltimo() const;
-    bool estaVazia() const;
-    int tamanho() const;
-    void limpar();
+        //Métodos de Inserção e Remoção
+        void inserirInicio(T valor);
+        void inserirFim(T valor);
+        T removerInicio();
+        T removerFim();
+        
+        // Métodos de Acesso
+        T obterPrimeiro() const;
+        T obterUltimo() const;
 
-    // Métodos extras para funcionar como fila
-    void push_back(T valor) { inserirFim(valor); }
-    void pop_front() { removerInicio(); }
-    T front() const { return obterPrimeiro(); }
-    bool empty() const { return estaVazia(); }
+        // Métodos de Verificação
+        bool estaVazia() const;
+        int getTamanho() const;
+        void limpar();
 };
 
-#endif // LISTAENCADEADA_HPP
+//Implementação dos métodos da Lista Encadeada
+
+//Construtor
+template <typename T>
+ListaEncadeada<T>::ListaEncadeada() : cabeca(nullptr), cauda(nullptr), tamanho(0) {}
+
+//Destrutor
+template <typename T>
+ListaEncadeada<T>::~ListaEncadeada() { limpar(); }
+
+//Insercao no Inicio da Lista
+template <typename T>
+void ListaEncadeada<T>::inserirInicio(T valor) {
+    No<T>* novo = new No<T>(valor);
+    novo->proximo = cabeca;
+
+    if (cabeca != nullptr)
+        cabeca->anterior = novo;
+    cabeca = novo;
+    if (cauda == nullptr)
+        cauda = novo;
+
+    tamanho++;
+}
+
+//Insercao no Fim da Lista
+template <typename T>
+void ListaEncadeada<T>::inserirFim(T valor) {
+    No<T>* novo = new No<T>(valor);
+    novo->anterior = cauda;
+
+    if (cauda != nullptr)
+        cauda->proximo = novo;
+    cauda = novo;
+    if (cabeca == nullptr)
+        cabeca = novo;
+
+    tamanho++;
+}
+
+//Remocao no Inicio da Lista
+template <typename T>
+T ListaEncadeada<T>::removerInicio() {
+
+    if (estaVazia())
+        throw std::runtime_error("Lista vazia");
+
+    No<T>* temp = cabeca;
+    T val = temp->valor;
+    cabeca = cabeca->proximo;
+
+    if (cabeca != nullptr)
+        cabeca->anterior = nullptr;
+    else
+        cauda = nullptr;
+
+    delete temp;
+    tamanho--;
+    return val;
+}
+
+//Remocao no Fim da Lista
+template <typename T>
+T ListaEncadeada<T>::removerFim() {
+
+    if (estaVazia())
+        throw std::runtime_error("Lista vazia");
+
+    No<T>* temp = cauda;
+    T val = temp->valor;
+    cauda = cauda->anterior;
+
+    if (cauda != nullptr)
+        cauda->proximo = nullptr;
+    else
+        cabeca = nullptr;
+
+    delete temp;
+    tamanho--;
+    return val;
+}
+
+//Retorna o primeiro elemento da lista
+template <typename T>
+T ListaEncadeada<T>::obterPrimeiro() const {
+    if (estaVazia())
+        throw std::runtime_error("Lista vazia");
+
+    return cabeca->valor;
+}
+
+//Retorna o ultimo elemento da lista
+template <typename T>
+T ListaEncadeada<T>::obterUltimo() const {
+    if (estaVazia())
+        throw std::runtime_error("Lista vazia");
+
+    return cauda->valor;
+}
+
+// Verifica se a lista está vazia
+template <typename T>
+bool ListaEncadeada<T>::estaVazia() const {
+    return tamanho == 0;
+}
+
+// Retorna o tamanho da lista
+template <typename T>
+int ListaEncadeada<T>::getTamanho() const {
+    return tamanho;
+}
+
+//Remove todos os elementos da lista
+template <typename T>
+void ListaEncadeada<T>::limpar() {
+    while (!estaVazia())
+        removerInicio();
+}
+
+#endif
