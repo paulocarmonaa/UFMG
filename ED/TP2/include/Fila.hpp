5aa2fe65f8e5f17523e3e4b57b50632a5d5a34c9
Paulo Henrique <paulo.carmona17@gmail.com>
2025-06-21 17:24:16 -0300

    Adição do TAD Fila para fins de modularização

---
+++ ED/TP2/include/Fila.hpp
@@ -0,0 +1,55 @@
+#ifndef FILA_HPP
+#define FILA_HPP
+
+#include"ListaEncadeada.hpp"
+
+
+template <typename T>
+class Fila{
+    private:
+        ListaEncadeada<T> lista;
+    public:
+        //Construto e destrutor
+        Fila();
+        ~Fila();
+
+        //Métodos de Inserção e Remoção
+        void enfileirar(T valor);
+        T desenfileirar();
+
+        //Métodos de Acesso
+        int getTamanho() const;
+        T getFrente() const;
+        bool estaVazia() const;
+
+};
+
+//Construtor
+template <typename T>
+Fila<T>::Fila() : lista() {}
+
+//Destrutor
+template <typename T>
+Fila<T>::~Fila() {}
+
+//Método de Inserção
+template <typename T>
+void Fila<T>::enfileirar(T valor) { lista.inserirFim(valor); }
+
+//Método de Remoção
+template <typename T>
+T Fila<T>::desenfileirar() { return lista.removerInicio(); }
+
+//Retorna o tamanho da fila
+template <typename T>
+int Fila<T>::getTamanho() const { return lista.getTamanho(); }
+
+//Retorna o primeiro elemento da fila sem removê-lo
+template <typename T>
+T Fila<T>::getFrente() const { return lista.obterPrimeiro(); }
+
+//Verifica se a fila está vazia
+template <typename T>
+bool Fila<T>::estaVazia() const { return lista.estaVazia(); }
+
+#endif
